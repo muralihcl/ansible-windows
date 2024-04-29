d166d3b6c9a5ce85fd615bc228528eab7cf922d3
Muralidhara Kakkunje <user@example.com>
2024-04-29 12:18:26 +0530

    Keeping only the required contents

--- files/helloworld.ps1
+++
@@ -1,7 +0,0 @@
-#!powershell
-#Requires -Version 3.0
-# Filename: helloworld.ps1
-Write-Host
-Write-Host 'Hello World!'
-Write-Host "Good-bye World! `n"
-# end of script
--- files/workstation-install.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-SET DIR=%~dp0%
-@PowerShell -NoProfile -ExecutionPolicy unrestricted -Command "& '%DIR%workstation-setup.ps1' %*"  > install.log 2>&1
-pause
